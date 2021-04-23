baa2069818bd2ae5ab8455b2a8b17c6f5312bc57
srivedhbalguri <62438630+srivedhbalguri@users.noreply.github.com>
2021-04-23 13:36:34 -0400

    Update functions.php

--- wp-content/themes/child theme/functions.php
+++ wp-content/themes/child theme/functions.php
@@ -1,7 +1,10 @@
 <?php
 function child_theme() {
 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-wp_enqueue_style( 'child-style', get_stylesheet_uri(). '/style.css'  );
+wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', array('parent-style'));    
 }
 add_action( 'wp_enqueue_scripts', 'child_theme' );
 ?>
+
+<?php
+
